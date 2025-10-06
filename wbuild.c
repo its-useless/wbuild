5eaf928d212a6f256666d4e9d5818e724c0efb24
aceinetx <mouselin82@gmail.com>
2025-10-06 14:18:10 +0300

    upd

--- wbuild.c
+++ wbuild.c
@@ -2,12 +2,17 @@
 #include "wbuild.h"
 
 int main(int argc, char** argv) {
+    add_target("wbuild.c");
+    target_depends("wbuild.c", "wbuild.h");
+    target_commands("wbuild.c", "touch wbuild.c");
+
     add_target("wbuild.o");
-    target_depends("wbuild.o", "wbuild.c", "wbuild.h");
+    target_depends("wbuild.o", "wbuild.c");
     target_commands(
         "wbuild.o",
         "cc wbuild.c -o wbuild.o -c -g -fsanitize=address,leak"
     );
+
     add_target("wbuild");
     target_depends("wbuild", "wbuild.o");
     target_commands(
