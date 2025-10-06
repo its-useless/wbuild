4821e4adb037a646222cab2c756c99559f7aaac8
aceinetx <mouselin82@gmail.com>
2025-10-06 14:09:14 +0300

    updx

--- wbuild.c
+++ wbuild.c
@@ -2,11 +2,17 @@
 #include "wbuild.h"
 
 int main(int argc, char** argv) {
+    add_target("wbuild.o");
+    target_depends("wbuild.o", "wbuild.c", "wbuild.h");
+    target_commands(
+        "wbuild.o",
+        "cc wbuild.c -o wbuild.o -c -g -fsanitize=address,leak"
+    );
     add_target("wbuild");
-    target_depends("wbuild", "wbuild.c", "wbuild.h");
+    target_depends("wbuild", "wbuild.o");
     target_commands(
         "wbuild",
-        "cc wbuild.c -o wbuild -g -fsanitize=address,leak"
+        "cc wbuild.o -o wbuild -g -fsanitize=address,leak"
     );
     return 0;
 }
